d7518d9579d5621d14a36a5729ae632dd9cc43f5
Marcio <marcio@touchable.dev>
2021-05-15 20:34:46 -0300

    Initial Commit

---
+++ MarcioCard/MarcioCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarcioCardApp.swift
+//  MarcioCard
+//
+//  Created by Marcio Silva on 15/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MarcioCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
