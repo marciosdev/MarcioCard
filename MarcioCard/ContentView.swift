eac6e411e2193c081f3e08609711ad5b43f7b8c5
Marcio <marcio@touchable.dev>
2021-05-20 22:50:37 -0300

    Project finally

--- MarcioCard/ContentView.swift
+++ MarcioCard/ContentView.swift
@@ -9,13 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(red:0.09, green: 0.6, blue:0.52)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("memoji")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5)
+                    )
+                Text("MARCiOSDEV")
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .padding()
+                    .font(.title)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                ExtractedView(text: "+55 48 99664 6300")
+            }
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ExtractedView: View {
+    
+    let text: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/40.0/*@END_MENU_TOKEN@*/)
+            .fill(Color.white)
+            .frame(height: 50.0)
+            .overlay(Text(text).foregroundColor(.black))
+            .padding()
     }
 }
